8c937f0652eaddd4f003fa1381e383f66fa5a6c3
Mradul Mishra <mmradul203@gmail.com>
2026-05-07 09:29:50 +0530

    initial changes

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Home Page</h1>
+      <h1 className="text-red-500">Home Page</h1>
+      <p>Welcome to the home page of our Next.js application!</p>
+    </div>
+  );
+};
+
+export default page;
