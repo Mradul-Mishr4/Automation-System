4953631eddb4d61fe01eba32b564194d0f1cba66
Mradul Mishra <mmradul203@gmail.com>
2026-05-26 20:49:05 +0530

    test github webhook

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,11 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+// repeating this code for webhook checking
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
